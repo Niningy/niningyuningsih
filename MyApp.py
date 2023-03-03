cc6e6fa2166c171efdd44e8d1be013c10f7e1479
Niningy <116931859+Niningy@users.noreply.github.com>
2023-03-03 17:43:57 +0700

    Add files via upload

---
+++ MyApp.py
@@ -0,0 +1,35 @@
+#import the required Libraries streamlit import st
+
+import streamlit as st
+import pandas as pd
+import os
+
+# Add a tittle and intro text
+st.title('CURRICULUM VITAE')
+
+st.title('NINING YUNINGSIH')
+
+st.text('ADUCATION')
+st.text('Universitas Tebuka Bachelor of Accounting 2018-2023')
+st.text('High School SMA PANDU IPA Passed 2011')
+st.text('MTS TSANAWIAH Passed 2008')
+st.text('MI SIRAJUL HUDA Passed 2007')
+
+st.text('NON FORMAL EDUCATION')
+st.text('1. Training Course Computer High School')
+st.text('2. Chief of Kadiv.Progrram Seminar Education Youth Meeting')
+st.text('3. English Club')
+st.text('4. TOEFL')
+st.text('5. Training Course online SAP MM and SAP Netweaver ABAP Program')
+st.text('6.Training Course Python Progrraming')
+
+
+st.text('JOB EXPERIENCES')
+st.text('1.Briton School Of English (BSE) Course and Tranlation Bording Position Administration - South Jakarta 2012-2013')
+st.text('2.PT.Garuda Ekspress Delivary (Ekspedition Company) Customer services & Sales Outlate Position South Jakarta 2013-2014')
+st.text('3.PT.Carefastindo Last Position Administrasi Support Logistic West Jakarta 2014-2020')
+st.text('4.PT.HIJAU BERDIKARI INDONESIA last Position Administrasi Opeerational')
+st.text('5.Founder Wijayakusumasport online shop umkm Bogor West Java 2020-2021')
+st.text('6.Sales\Marketing Online 2021-Now')
+st.text('7.freelancer Tutor in the PKBM Bording School 2022-Now')
+
